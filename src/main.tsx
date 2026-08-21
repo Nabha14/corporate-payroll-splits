24f6e7a321807127ec4d09ea4310bb3ffd34a8c8
Nabha14 <192114902+Nabha14@users.noreply.github.com>
2026-08-21 12:12:12 +0530

    feat: add recoverable production failure state

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import PayrollSafetyBoundary from './PayrollSafetyBoundary.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PayrollSafetyBoundary>
+      <App />
+    </PayrollSafetyBoundary>
   </React.StrictMode>
 );
